dde3bf8383c3cd7c83742fd33eb1052305e7a8fd
kevinjapa <84851902+kevinjapa@users.noreply.github.com>
2021-06-01 22:13:51 -0500

    Add files via upload

---
+++ matrizInverza/src/matrizinverza/MatrizInverza.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package matrizinverza;
+
+import java.util.*;
+
+/**
+ *
+ * @author japak
+ */
+public class MatrizInverza {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner teclado=new Scanner(System.in);
+        System.out.println("ingrese el tamaño del vector");
+        int tamanio=teclado.nextInt();
+        int vector[]=new int [tamanio] ;
+        for(int i=0;i<vector.length;i++)
+        {
+            
+        }
+    }
+    
+}
