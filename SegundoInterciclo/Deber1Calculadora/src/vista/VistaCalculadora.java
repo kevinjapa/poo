d0c4ac376d122212c4cbdaa6a1e2dabe62e4fc63
kevinjapa <84851902+kevinjapa@users.noreply.github.com>
2021-06-15 00:28:15 -0500

    Add files via upload

--- SegundoInterciclo/Deber1Calculadora/src/vista/VistaCalculadora.java
+++ SegundoInterciclo/Deber1Calculadora/src/vista/VistaCalculadora.java
@@ -27,18 +27,16 @@ import javax.swing.JTextField;
  */
 public class VistaCalculadora extends JFrame
 {
-    ArrayList lista=new ArrayList();
-    JPanel panelPrincipal;
-    JPanel panel;
+    
+    JPanel panelPrincipal,panel;
     JButton boton;
     JTextField cuadroTexto=new JTextField("");
-    int a,b;
+    int a,b,operacion;
     String op;
-    int operacion;
     public VistaCalculadora()
     {
         super();
-        setTitle("Calculadora");
+        setTitle("-----Calculadora---------");
         setBounds(100,100,500,500);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         initComponentes();
@@ -48,7 +46,7 @@ public class VistaCalculadora extends JFrame
     }
     public JPanel initCalculadora()
     {
-        
+        ArrayList lista=new ArrayList();
         panelPrincipal=new  JPanel();
         GridLayout tablaPlantilla=new GridLayout(4,5);
         panelPrincipal.setLayout(tablaPlantilla);
@@ -58,42 +56,23 @@ public class VistaCalculadora extends JFrame
             for(int j=0; j<tablaPlantilla.getColumns();j++)
             {
                 String texto =""+contador;
-                if(contador==4)
-                {
-                    texto="-";
-                }
-                else if(contador==5)
-                {
-                    texto="C";
-                }
-                else if(contador==9)
-                {
-                    texto="+";
-                }
-                else if(contador==10)
-                {
-                    texto="";
-                }
-                else if(contador==14)
-                {
-                    texto="*";
-                }
-                else if(contador==15)
-                {
-                    texto="";
-                }
-                else if(contador==17)
-                {
-                    texto="";
-                }
-                else if(contador==19)
-                {
-                    texto="/";
-                }
-                else if(contador==20)
-                {
-                    texto="=";
-                }
+                if(contador==4)texto="-";
+                else if(contador==5)texto="C";
+                else if(contador==6)texto="4";
+                else if(contador==7)texto="5";
+                else if(contador==8)texto="6";
+                else if(contador==9)texto="+";
+                else if(contador==10)texto="/";
+                else if(contador==11)texto="7";
+                else if(contador==12)texto="8";
+                else if(contador==13)texto="9";
+                else if(contador==14)texto="*";
+                else if(contador==15)texto="=";
+                else if(contador==16)texto="";
+                else if(contador==17)texto="0";
+                else if(contador==18)texto="";
+                else if(contador==19)texto="";
+                else if(contador==20)texto="";
                 boton=new JButton(texto);
                 panelPrincipal.add(boton);
                 //leee lo del botnm
@@ -105,7 +84,8 @@ public class VistaCalculadora extends JFrame
                         //v1=0;
                         if(btn.getText().equals("C"))
                         {
-                           JOptionPane.showMessageDialog(panelPrincipal,"REINICIADO");
+                            initCalculadora();
+                           //JOptionPane.showMessageDialog(panelPrincipal,"REINICIADO");
                         }
                         else if(btn.getText().equals("=")&&op.equals("+"))
                         {
@@ -116,13 +96,66 @@ public class VistaCalculadora extends JFrame
                                     a=Integer.parseInt((String)lista.get(i));
                                     b=Integer.parseInt((String)lista.get(j));
                                     operacion=a+b;
-                                    //lista.add(operacion);
-                                    System.out.println("la suma es:"+operacion);
                                     j++;
                                 }
                                 
                             }
-                            
+                            lista.add(""+operacion);
+                            System.out.println("suma:"+operacion);
+                        }
+                        else if(btn.getText().equals("=")&&op.equals("-"))
+                        {
+                            for(int i=0; i<lista.size();i++)
+                            {
+                                for(int j=i+1;j<lista.size();i++)
+                                {
+                                    a=Integer.parseInt((String)lista.get(i));
+                                    b=Integer.parseInt((String)lista.get(j));
+                                    operacion=a-b;
+                                    //lista.add(operacion);
+                                    
+                                    j++;
+                                }
+                                
+                            }
+                            lista.add(""+operacion);
+                            System.out.println("resta:"+operacion);
+                        }
+                        else if(btn.getText().equals("=")&&op.equals("*"))
+                        {
+                            for(int i=0; i<lista.size();i++)
+                            {
+                                for(int j=i+1;j<lista.size();i++)
+                                {
+                                    a=Integer.parseInt((String)lista.get(i));
+                                    b=Integer.parseInt((String)lista.get(j));
+                                    operacion=a*b;
+                                    //lista.add(operacion);
+                                    
+                                    j++;
+                                }
+                                
+                            }
+                            lista.add(""+operacion);
+                            System.out.println("multiplicacion:"+operacion);
+                        }
+                        else if(btn.getText().equals("=")&&op.equals("/"))
+                        {
+                            for(int i=0; i<lista.size();i++)
+                            {
+                                for(int j=i+1;j<lista.size();i++)
+                                {
+                                    a=Integer.parseInt((String)lista.get(i));
+                                    b=Integer.parseInt((String)lista.get(j));
+                                    operacion=a/b;
+                                    //lista.add(operacion);
+                                    
+                                    j++;
+                                }
+                                
+                            }
+                            lista.add(""+operacion);
+                            System.out.println("divicions:"+operacion);
                         }
                         else if(btn.getText().equals("+"))
                         {
@@ -149,7 +182,7 @@ public class VistaCalculadora extends JFrame
                         else 
                         {
                             lista.add(btn.getText());
-                            System.out.println("arreglo"+lista);
+                            //System.out.println("arreglo"+lista);
                         }
                         
                     }
